5fb3286a2ca0a1ed470f81f2e7265b76620daf21
dperuo <derek@derekperuo.net>
2018-04-04 08:49:11 -0500

    Update main.js with es6 formatting

--- js/service-workers/main.js
+++ js/service-workers/main.js
@@ -1,19 +1,45 @@
-(function () {
-  if (window.Worker) {
-    var worker = new Worker('worker.js');
+((undefined) => {
 
-    worker.postMessage({
-      arrayLength: 500000
-    });
+  console.log('MAIN: main.js working...')
 
-    console.log('main.js working...');
-
-    worker.onmessage = getArray;
-
-    function getArray(e) {
-      console.log('worker.js finished!', e.data);
-    };
-
-    console.log('main.js still working...');
+  if ('serviceWorker' in navigator) {
+    runScript();
+  } else {
+    runFallback();
   }
+
+  ////////////
+
+  function runScript () {
+    window.navigator.serviceWorker.register('./service-worker.js')
+      .then(() => console.log('MAIN: service worker installed'))
+      .catch(err => console.log('MAIN: Error', err));
+  }
+
+  function runFallback () {
+    console.log('MAIN: runFallback executed!')
+  }
+
 })();
+
+
+
+// (function () {
+//   if (window.Worker) {
+//     var worker = new Worker('worker.js');
+
+//     worker.postMessage({
+//       arrayLength: 500000
+//     });
+
+//     console.log('main.js working...');
+
+//     worker.onmessage = getArray;
+
+//     function getArray(e) {
+//       console.log('worker.js finished!', e.data);
+//     };
+
+//     console.log('main.js still working...');
+//   }
+// })();
