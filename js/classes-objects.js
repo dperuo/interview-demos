692b25cf8a85907019aaa9358f6fddf712dfad62
dperuo <derek@derekperuo.net>
2018-05-02 10:37:13 -0500

    Compile TypeScript

--- js/classes-objects.js
+++ js/classes-objects.js
@@ -1,31 +1,29 @@
 /**
  * Classes and Objects
  *
- * Classes are templates for objects and define object properties and
+ * Classes are templates for objects. They define object properties and
  * behaviors.
  */
-var GalaxyShip = (function () {
-    function GalaxyShip(_a) {
-        var name = _a.name, registry = _a.registry, captain = _a.captain;
+class GalaxyShip {
+    constructor({ name, registry, captain }) {
         this.info = {
-            captain: captain,
+            captain,
             decks: 42,
             maxCrew: 6000,
             maxWarp: 9.8,
-            name: name,
-            registry: registry,
+            name,
+            registry,
             type: "Explorer"
         };
     }
-    GalaxyShip.prototype.get = function (item) {
+    get(item) {
         return (item) ? this.info[item] : this.info;
-    };
-    return GalaxyShip;
-}());
+    }
+}
 /**
  * Objects are instances of classes and inherit class properties and methods
  */
-var enterprise = new GalaxyShip({
+let enterprise = new GalaxyShip({
     captain: "Picard",
     name: "Enterprise D",
     registry: "NCC-1701-D"
