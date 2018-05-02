3e7166fb5f6806b27b0045a5bf2763148c0e9ff9
dperuo <derek@derekperuo.net>
2018-05-02 10:55:36 -0500

    Move try-catch file

--- ts/try-catch.ts
+++ ts/try-catch.ts
@@ -6,22 +6,25 @@
  *
  * The try block executes as writen if no exception is thrown.
  */
-const array = [1, 2, 3, 0, 4, 5];
+
+const array = [1, 2, 3, 0, 4, 5]
+
 try {
-    if (notDefined) {
-        const result = true;
-    }
+ if (notDefined) {
+   const result = true
+ }
+} catch (e) {
+  throw Error(e)
 }
-catch (e) {
-    throw Error(e);
-}
+
 // => Uncaught Error: ReferenceError: notAnArray is not defined
+
 try {
-    if (array) {
-        const bool = true;
-    }
+ if (array) {
+   const bool = true
+ }
+} catch (e) {
+  throw Error(e)
 }
-catch (e) {
-    throw Error(e);
-}
+
 // => bool = true
